e82c8b54faf5cc431a0bdfed63fd9a4c0389b1eb
Zach5812 <zachlouk7@gmail.com>
2023-06-17 17:12:25 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using LoginReg.Models;
+
+
+namespace LoginReg.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private MyContext db;
+
+    public HomeController(ILogger<HomeController> logger, MyContext context)
+    {
+        _logger = logger;
+        db = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View("Index");
+    }
+
+    [HttpGet("Success")]
+
+    public IActionResult Success()
+    {
+        return View("Success");
+    }
+    [HttpPost("register")]
+    public IActionResult Register(User newUser)
+    {
+        if (!ModelState.IsValid)
+        {
+            return Index();
+        }
+        PasswordHasher<User> hashedPW = new PasswordHasher<User>();
+        newUser.Password = hashedPW.HashPassword(newUser, newUser.Password);
+        db.Users.Add(newUser);
+        db.SaveChanges();
+
+        HttpContext.Session.SetInt32("UUID", newUser.UserId);
+        return RedirectToAction("Success");
+    }
+
+    [HttpPost("login")]
+
+    public IActionResult Login(LoginUser loginUser)
+    {
+        if (!ModelState.IsValid)
+        {
+            return Index();
+        }
+        User? dbUser = db.Users.FirstOrDefault(user => user.Email == loginUser.LoginEmail);
+
+        if (dbUser == null)
+        {
+            ModelState.AddModelError("LoginEmail", "does not match");
+            return Index();
+        }
+
+        PasswordHasher<LoginUser> hashedPW = new PasswordHasher<LoginUser>();
+        PasswordVerificationResult pwCompare = hashedPW.VerifyHashedPassword(loginUser, dbUser.Password, loginUser.LoginPassword);
+
+        if(pwCompare == 0)
+        {
+            ModelState.AddModelError("LoginPassword", "does not match");
+        }
+
+        HttpContext.Session.SetInt32("UUID", dbUser.UserId);
+        return RedirectToAction("Success");
+    }
+
+[HttpPost("logout")]
+public IActionResult Logout()
+{
+    HttpContext.Session.Clear();
+    return RedirectToAction("Index");
+}
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/LoginUser.cs
@@ -0,0 +1,16 @@
+#pragma warning disable CS8618
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[NotMapped]
+public class LoginUser
+{
+    [Required]
+    [EmailAddress]
+    public string LoginEmail {get; set;}
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string LoginPassword {get; set;}
+}
